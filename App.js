fb80a9b96ccb58dc1d59176e84c0dbf8e054ad49
aryankapoorr <aryan.kapoor.1102@gmail.com>
2022-10-01 16:33:49 -0500

    adding base files

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ 
+ 
+ const App = () => {
+   return (
+     <SafeAreaView>
+       <Text>Hello World</Text>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   
+ });
+ 
+ export default App;
+ 
