d90fc8e7758c4d9c329f7459e1d73ca48228bcd0
Vy Nguyen <vypnguyen2003@gmail.com>
2022-10-18 20:39:43 -0500

    Added App.js

--- App.js
+++ App.js
@@ -1,35 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- 
- 
- 
- const App = () => {
-   return (
-     <SafeAreaView>
-       <Text>Hello World</Text>
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
-   
- });
- 
- export default App;
- 
+
+import HomeNoListScreen from './src/screens/HomeNoList';
+import SearchFriendsScreen from './src/screens/SearchFriends';
+import MyFriendsScreen from './src/screens/MyFriends';
+import ProfileScreen from './src/screens/Profile';
+import MenuBar from './src/components/MenuBar';
+import ResetPassword1 from './src/screens/ResetPassword1';
+import ResetPassword2 from './src/screens/ResetPassword2';
+import EditProfile from './src/screens/EditProfile';
+import LandingPage from './src/screens/LandingPage';
+import SignUp from './src/screens/SignUp';
+import LogIn from './src/screens/LogIn';
+import NewList from './src/screens/NewList';
+import InviteFriends from './src/components/InviteFriends';
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MenuBar />
+    </NavigationContainer>
+  );
+}
+
+export default App;
