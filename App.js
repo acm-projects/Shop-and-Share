fbefcd9422c05db9fb2a556fead55b5f9abe2c2d
Vy Nguyen <vypnguyen2003@gmail.com>
2022-10-27 15:46:23 -0500

    Fixed App.js

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
