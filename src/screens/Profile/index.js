7426c23fd140889360e30e61e79595e198e0be11
aryankapoorr <aryan.kapoor.1102@gmail.com>
2022-11-05 18:40:26 -0500

    ⚗️ profile page functionality

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,141 +1,169 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  SafeAreaView, 
-  TouchableOpacity, 
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
   Image,
   ImageBackground,
-}  from "react-native";
+} from "react-native";
 
 import styles from './styles.js';
+import { firebase } from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import { useNavigation } from "@react-navigation/core";
 import EditProfile from '../EditProfile/index.js';
 
+const db = firestore().collection('Users');
+
 const ProfileScreen = () => {
-    return (
-      <SafeAreaView>
+  const user = firebase.auth().currentUser?.email;
+  const nav = useNavigation();
+
+  const [name, setName] = React.useState('');
+  const [lists, setLists] = React.useState(0);
+
+  React.useEffect(() => {
+    db.doc(user).get().then(query => {
+      setName(query.data().firstName)
+    })
+    getCount();
+  }, [])
+
+  const getCount = async () => {
+    setLists((await db.doc(user).collection('Lists').count().get()).data().count - 1);
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut().then(() => {
+      nav.pop();
+    })
+  }
+
+  return (
+    <SafeAreaView>
       <ImageBackground source={require('../../../assets/images/Background.jpg')}
         style={styles.ImageBackground}>
         <View>
           <SafeAreaView>
-                <Text style={styles.WelcomeMessage}>
-                    Welcome Back, 
+            <Text style={styles.WelcomeMessage}>
+              Welcome Back,
+            </Text>
+            {/* add functionality to change this first name/display name*/}
+            <Text style={styles.FirstName}>
+              {name}
+            </Text>
+            <Text style={styles.ExclamationPoint}>
+              !
+            </Text>
+
+            {/* add functionality to change the profile image later */}
+            <View style={styles.ProfilePhotoShadow}>
+            </View>
+
+            <Image
+              style={styles.ProfilePhoto}
+              source={require('../../../assets/images/Temporary_Profile_Photo.jpg')} />
+
+            {/* add functionality to the number of lists and groups later*/}
+            <View style={styles.NumbersContainer}>
+              <Text style={styles.ListsNumber}>
+                {lists}
+              </Text>
+              {/* <Text style={styles.GroupsNumber}>
+                15
+              </Text> */}
+            </View>
+
+            <View>
+              <Text style={styles.ListsText}>
+                Lists
+              </Text>
+              {/* <Text style={styles.GroupsText}>
+                Groups
+              </Text> */}
+            </View>
+
+            {/* onPress -> open edit profile component */}
+            <View style={styles.EditProfileContainer}>
+              <TouchableOpacity onPress={() => nav.push('Edit Profile')}>
+                <Text style={styles.EditProfileButton}>
+                  Edit Profile
                 </Text>
-              {/* add functionality to change this first name/display name*/}
-                <Text style={styles.FirstName}>
-                    Melissa
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.ProfileDetailsContainer}>
+            </View>
+
+            <Text style={styles.ProfileHeader}>
+              PROFILE
+            </Text>
+
+            <View style={styles.positionButtons}>
+              <View style={styles.DividerThick} />
+
+              {/* fix navigation onPress for these buttons */}
+              <TouchableOpacity
+                onPress={() => navigation.navigate('My Home')}
+              >
+                <Text style={styles.ButtonFormat}>
+                  My Home
                 </Text>
-                <Text style={styles.ExclamationPoint}>
-                    !
+                <Image
+                  style={styles.ButtonArrows}
+                  source={require('../../../assets/images/Profile_Button_Arrows.png')} />
+              </TouchableOpacity>
+
+              <View style={styles.DividerThin} />
+
+              <TouchableOpacity
+                onPress={() => nav.push('View Dietary Restrictions')}
+              >
+                <Text style={styles.ButtonFormat}>
+                  Dietary Restrictions
                 </Text>
+                <Image
+                  style={styles.ButtonArrows}
+                  source={require('../../../assets/images/Profile_Button_Arrows.png')} />
+              </TouchableOpacity>
 
-                {/* add functionality to change the profile image later */}
-                <View style={styles.ProfilePhotoShadow}>
-                </View>
+              <View style={styles.DividerThick} />
 
-                <Image 
-                style={styles.ProfilePhoto}
-                source={require('../../../assets/images/Temporary_Profile_Photo.jpg')}/>  
-
-                {/* add functionality to the number of lists and groups later*/}
-                <View style={styles.NumbersContainer}>
-                  <Text style={styles.ListsNumber}>
-                    72
-                  </Text>
-                  <Text style={styles.GroupsNumber}>
-                    15
-                  </Text>
-                </View>
-
-                <View>
-                  <Text style={styles.ListsText}>
-                    Lists
-                  </Text>
-                  <Text style={styles.GroupsText}>
-                    Groups
-                  </Text>
-                </View>
-
-                {/* onPress -> open edit profile component */}
-                <View style={styles.EditProfileContainer}>
-                <TouchableOpacity>
-                    <Text style={styles.EditProfileButton}>
-                        Edit Profile
-                    </Text>  
-                </TouchableOpacity>
-              </View>
-
-                <View style={styles.ProfileDetailsContainer}>
-                </View>
-
-                <Text style={styles.ProfileHeader}>
-                  PROFILE
+              <TouchableOpacity
+                onPress={() => nav.push('Reset Password 1')}
+              >
+                <Text style={styles.ButtonFormat}>
+                  Reset Password
                 </Text>
+                <Image
+                  style={styles.ButtonArrows}
+                  source={require('../../../assets/images/Profile_Button_Arrows.png')} />
+              </TouchableOpacity>
 
-                <View style={styles.positionButtons}>
-                    <View style={styles.DividerThick}/>
-                    
-                {/* fix navigation onPress for these buttons */}
-                  <TouchableOpacity
-                      onPress={() => navigation.navigate('My Home')}
-                      >
-                      <Text style={styles.ButtonFormat}>
-                          My Home
-                      </Text>
-                      <Image 
-                          style={styles.ButtonArrows}
-                          source={require('../../../assets/images/Profile_Button_Arrows.png')}/>
-                  </TouchableOpacity>
+              <View style={styles.DividerThick} />
 
-                  <View style={styles.DividerThin}/>
+              <TouchableOpacity
+                onPress={handleSignOut}
+              >
+                <Text style={styles.SignOutButton}>
+                  Sign Out
+                </Text>
+                <Image
+                  style={styles.ButtonArrows}
+                  source={require('../../../assets/images/Profile_Button_Arrows.png')} />
+              </TouchableOpacity>
 
-                  <TouchableOpacity
-                      onPress={() => navigation.navigate('View Dietary Restrictions')}
-                      >
-                      <Text style={styles.ButtonFormat}>
-                          Dietary Restrictions
-                      </Text>
-                      <Image 
-                          style={styles.ButtonArrows}
-                          source={require('../../../assets/images/Profile_Button_Arrows.png')}/>
-                  </TouchableOpacity>
-
-                    <View style={styles.DividerThick}/>
-
-                  <TouchableOpacity
-                      onPress={() => navigation.navigate('Reset Password')}
-                      >
-                      <Text style={styles.ButtonFormat}>
-                          Reset Password
-                      </Text>
-                      <Image 
-                          style={styles.ButtonArrows}
-                          source={require('../../../assets/images/Profile_Button_Arrows.png')}/>
-                  </TouchableOpacity>
-
-                    <View style={styles.DividerThick}/>
-
-                  <TouchableOpacity
-                      onPress={() => navigation.navigate('Sign Out')}
-                      >
-                      <Text style={styles.SignOutButton}>
-                          Sign Out
-                      </Text>
-                      <Image 
-                          style={styles.ButtonArrows}
-                          source={require('../../../assets/images/Profile_Button_Arrows.png')}/>
-                  </TouchableOpacity>
-
-                   <View style={styles.DividerThick}/>
-                </View>
+              <View style={styles.DividerThick} />
+            </View>
           </SafeAreaView>
         </View>
-        </ImageBackground>
-      </SafeAreaView>
-    )
-  }
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
 
-  export default ProfileScreen;
+export default ProfileScreen;
--- src/screens/ViewDietaryRestrictions/index.js
+++ src/screens/ViewDietaryRestrictions/index.js
@@ -1,84 +1,121 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  SafeAreaView, 
-  TouchableOpacity, 
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
   Image,
   ImageBackground,
-}  from "react-native";
+} from "react-native";
 
+import firestore from '@react-native-firebase/firestore';
+import { useNavigation } from "@react-navigation/core";
+import { firebase } from '@react-native-firebase/auth';
 import colors from '../../../assets/colors/colors.js';
 import styles from './styles.js';
 
+
 const ViewDietaryRestrictions = () => {
-    return (
-      <>
+  const nav = useNavigation();
+  const user = firestore().collection('Users').doc(firebase.auth().currentUser?.email);
+
+  const [list, setList] = React.useState([]);
+
+  React.useLayoutEffect(() => {
+    createList();
+  }, [])
+
+  const createList = () => {
+    user.get().then(query => {
+      setList(query.data().dietaryRestrictions);
+
+      console.log(list);
+    })
+  }
+
+  return (
+    <>
       <ImageBackground source={require('../../../assets/images/Background.jpg')}
         style={styles.ImageBackground}>
         <View>
           <SafeAreaView>
 
-              <View style={styles.EditProfileContainer}>
+            <View style={styles.EditProfileContainer}>
+              <TouchableOpacity
+                onPress={() => nav.pop()}
+              >
+                <Text style={styles.cancelText}>
+                  Cancel
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.Title}>
+              My Dietary Restrictions
+            </Text>
+
+            {
+              list.map((item) =>
                 <TouchableOpacity
-                    onPress={() => navigation.navigate('HomeNoList')}
+                  key={item} onPress={() => { }}
                 >
-                    <Text style={styles.cancelText}>
-                    Cancel
+                  <View style={styles.nutContainer}>
+                    <Text style={styles.Text}>
+                      {item}
                     </Text>
+                    {/* <Text style={styles.Description}>
+                      Peanuts
+                    </Text> */}
+                  </View>
                 </TouchableOpacity>
+              )
+            }
+
+            {/* <TouchableOpacity
+              onPress={() => { }}
+            >
+              <View style={styles.nutContainer}>
+                <Text style={styles.Text}>
+                  Nut Allergy
+                </Text>
+                <Text style={styles.Description}>
+                  Peanuts
+                </Text>
               </View>
+            </TouchableOpacity>
 
-                <Text style={styles.Title}>
-                    My Dietary Restrictions
-                    </Text>
-
-                <TouchableOpacity
-                onPress={() => {}}
-                >
-                    <View style={styles.nutContainer}>
-                      <Text style={styles.Text}>
-                          Nut Allergy
-                      </Text>  
-                      <Text style={styles.Description}>
-                          Peanuts
-                      </Text> 
-                </View>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => {}}
-                    >
-                <View style={styles.fishContainer}>
-                      <Text style={styles.Text}>
-                          Fish Allergy
-                      </Text>  
-                      <Text style={styles.Description}>
-                            Salmon
-                      </Text>  
-                </View>
-                </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => { }}
+            >
+              <View style={styles.fishContainer}>
+                <Text style={styles.Text}>
+                  Fish Allergy
+                </Text>
+                <Text style={styles.Description}>
+                  Salmon
+                </Text>
+              </View>
+            </TouchableOpacity>
 
 
-                <TouchableOpacity
-                    onPress={() => {}}
-                    >
-                <View style={styles.vegContainer}>
-                      <Text style={styles.Text}>
-                          Vegetarian
-                      </Text>  
-                </View>
-                </TouchableOpacity>
-
+            <TouchableOpacity
+              onPress={() => { }}
+            >
+              <View style={styles.vegContainer}>
+                <Text style={styles.Text}>
+                  Vegetarian
+                </Text>
+              </View>
+            </TouchableOpacity> */}
 
           </SafeAreaView>
         </View>
-        </ImageBackground>
-      </>
-    )
-  }
+      </ImageBackground>
+    </>
+  )
+}
 
-  export default ViewDietaryRestrictions;
+export default ViewDietaryRestrictions;
