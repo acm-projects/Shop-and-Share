0075c802184886440a610f7e58130d94641fc7c8
Vy Nguyen <vypnguyen2003@gmail.com>
2022-11-27 05:04:00 -0600

    accept/decline friend requests

--- src/screens/MyFriends/index.js
+++ src/screens/MyFriends/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import SearchableBar from '../../components/SearchBar.js';
 import InviteFriends from '../../components/InviteFriends.js';
+import { useState } from 'react';
 
 import { 
   StyleSheet, 
@@ -9,7 +10,8 @@ import {
   SafeAreaView, 
   TouchableOpacity, 
   ImageBackground,
-  Image
+  Image,
+  FlatList
 }  from "react-native";
 
 import styles from './styles.js';
@@ -22,6 +24,102 @@ const MyFriendsScreen = () => {
   let [flag, setFlag] = React.useState(true);
   let changeImage = () => setFlag(previousState => !previousState);
   let imagePath = flag ? friendRemove : friendRemoved
+
+  const [requests, changeRequests]  = useState([
+    { id : 1, name : "request1"},
+  ]);
+  const [friends, changeFriends]  = useState([
+  ]);
+  
+  const oneRequest = ( {item} ) => (
+    <View style={styles.AlignWithButton}>
+    <View style={styles.AlignItems}>
+        <Image source={require('../../../assets/images/Temporary_Profile_Photo.jpg')}
+            style={styles.FriendProfilePhoto}/>
+    <View>
+        <Text style={styles.DisplayName}>
+            Melissa Harper
+        </Text>
+        <Text style={styles.UserName}>
+            m.harper
+        </Text>
+    </View>
+    </View>
+      <TouchableOpacity onPress={() => onAccept()}
+          style={{marginLeft: 35}}>
+      <Image source={require('../../../assets/images/Accept_Button_White.png')}
+            style={styles.RemoveButton}/>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => onDecline()}>
+      <Image source={require('../../../assets/images/Decline_Button_Black.png')}
+            style={styles.RemoveButton}/>
+    </TouchableOpacity>
+    </View>
+)
+
+const oneFriend = ( {item} ) => (
+  <View style={styles.AlignWithButton}>
+  <View style={styles.AlignItems}>
+      <Image source={require('../../../assets/images/Temporary_Profile_Photo.jpg')}
+          style={styles.FriendProfilePhoto}/>
+  <View>
+      <Text style={styles.DisplayName}>
+          Melissa Harper
+      </Text>
+      <Text style={styles.UserName}>
+          m.harper
+      </Text>
+  </View>
+  </View>
+
+  {/*on first press, remove friend; on second press, add friend again*/}
+  <TouchableOpacity onPress={ () => changeImage() }>
+  <Image source={imagePath}
+        style={styles.RemoveButton}/>
+  </TouchableOpacity>
+  </View>
+)
+  
+  const [requestState, setrequestState] = useState(requests)
+  const [idx, incr] = useState(2);
+  const [friendState, setfriendState] = useState(friends)
+
+  const noRequestsMessage = ( {item} ) => {
+    return (
+      <View style={styles.AlignWithButton}>
+        <View>
+          <Text style={styles.EmptyRequests}>
+              You have no friend requests!
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
+  const onAccept = () => {
+    requests.pop({
+      id: 1,
+      name: "request1"
+    });
+    friends.push({
+      id: 1,
+      name: "request1"
+    })
+    var newRequests = [...requests]
+    setrequestState(newRequests)
+    changeRequests(newRequests)
+  }
+
+  const onDecline = () => {
+    requests.pop({
+      id: 1,
+      name: "request1"
+    });
+    var newRequests = [...requests]
+    setrequestState(newRequests)
+    changeRequests(newRequests)
+  }
+
     return (
       <SafeAreaView>
       <ImageBackground source={require('../../../assets/images/Background.jpg')}
@@ -42,32 +140,12 @@ const MyFriendsScreen = () => {
                 <Text style={styles.Header}>
                     FRIEND REQUESTS
                 </Text>
-
-                <View style={styles.AlignWithButton}>
-                  <View style={styles.AlignItems}>
-                      <Image source={require('../../../assets/images/Temporary_Profile_Photo.jpg')}
-                          style={styles.FriendProfilePhoto}/>
-                  <View>
-                      <Text style={styles.DisplayName}>
-                          Melissa Harper
-                      </Text>
-                      <Text style={styles.UserName}>
-                          m.harper
-                      </Text>
-                </View>
-              </View>
-                
-              {/*onPress for Accept, remove the friend request from the list and add friend; 
-              onPress for Decline, just remove the friend request from the list*/}
-              <TouchableOpacity style={{marginLeft: 35}}>
-                <Image source={require('../../../assets/images/Accept_Button_White.png')}
-                      style={styles.RemoveButton}/>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Image source={require('../../../assets/images/Decline_Button_Black.png')}
-                      style={styles.RemoveButton}/>
-              </TouchableOpacity>
-            </View>
+                <FlatList 
+                  nestedScrollEnabled
+                  data = { requestState }
+                  renderItem = { oneRequest }
+                  ListEmptyComponent={noRequestsMessage}
+                />
           </View>
 
 
@@ -77,28 +155,11 @@ const MyFriendsScreen = () => {
                     YOUR FRIENDS
                 </Text>
                 </View>
-
-                <View style={styles.AlignWithButton}>
-                  <View style={styles.AlignItems}>
-                      <Image source={require('../../../assets/images/Temporary_Profile_Photo.jpg')}
-                          style={styles.FriendProfilePhoto}/>
-                  <View>
-                      <Text style={styles.DisplayName}>
-                          Melissa Harper
-                      </Text>
-                      <Text style={styles.UserName}>
-                          m.harper
-                      </Text>
-                </View>
-              </View>
-                
-              {/*on first press, remove friend; on second press, add friend again*/}
-              <TouchableOpacity onPress={ () => changeImage() }>
-                <Image source={imagePath}
-                      style={styles.RemoveButton}/>
-              </TouchableOpacity>
-            </View>
-
+                <FlatList 
+                  nestedScrollEnabled
+                  data = { friendState }
+                  renderItem = { oneFriend }
+                />
             </View>
             </ScrollView>
           </SafeAreaView>
